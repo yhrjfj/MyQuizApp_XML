9388d23fddec64083d35bc9fd25d2a01d79ad272
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-10 20:08:15 +0600

    Switch MainActivity to QuizQuestionsActivity

--- app/src/main/java/com/yhr/jfj/myquizapp/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/myquizapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.yhr.jfj.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Quiz start button which is shown in the home screen
+        val btnStart: Button = findViewById(R.id.btn_start)
+
+        // Finding editText by it's id
+        val etName: EditText = findViewById(R.id.et_name)
+
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()){
+                Toast.makeText(this, "Enter your name", Toast.LENGTH_SHORT).show()
+            } else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
