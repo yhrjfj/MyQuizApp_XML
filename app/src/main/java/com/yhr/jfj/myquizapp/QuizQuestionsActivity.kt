15ddd6d964ee7860a768e4fef2cc8550d026b360
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-13 19:02:59 +0600

    Show answer as a option and change border color after select an option

--- app/src/main/java/com/yhr/jfj/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/yhr/jfj/myquizapp/QuizQuestionsActivity.kt
@@ -1,14 +1,23 @@
 package com.yhr.jfj.myquizapp
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    // Methods
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
     // Access element from activity_quiz_questions.xml
     private var progressBar: ProgressBar? = null
@@ -19,8 +28,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
+    private var btnSubmit: Button? = null
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
@@ -34,26 +43,119 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
 
+        mQuestionList = Constants.getQuestions()
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
+        setQuestion()
 
-        val currentPosition = 1
-        val question: Question = questionsList[currentPosition - 1]
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setQuestion() {
+
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
 
         // Set Question and Options with flag into the question screen
         tvQuestion?.text = question.question
         ivImage?.setImageResource(question.image)
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
+
+        // Option
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
 
+        // Set property in option button
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+
+        btnSubmit?.setOnClickListener(this)
+
+        // Change button text based on it's position
+        if (mCurrentPosition == mQuestionList!!.size) {
+            btnSubmit?.text = "Finish"
+        } else {
+            btnSubmit?.text = "Next"
+        }
+    }
+
+    /*
+     This function to set the default options view when the answer is loaded or ans was reset
+     Basically change the color of the color of the selected option
+    */
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+
+    }
+
+    /*
+        When user selected a option it change color
+    */
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+
+    }
+    /*
+        Onclick event for option selection
+     */
+    override fun onClick(view: View?) {
+        when(view?.id){
+            // Option 1
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+            // Option 2
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+            // Option 3
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+            // Option 4
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+            // Next/Finish button
+            R.id.btn_submit -> {
+                // TODO: "Implement"
+            }
+        }
     }
 }
